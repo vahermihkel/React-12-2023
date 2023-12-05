ab4c5c4a455b657bd987938a9e3ea8c42e788cb0
Mihkel Vaher <mihkel.vaher@industry62.com>
2023-12-05 13:08:18 +0200

    routing

--- veebipood/src/App.js
+++ veebipood/src/App.js
@@ -1,11 +1,30 @@
 //import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <button className="nupp">Nupp</button>
-      <img className="pilt" src="https://estonia.ee/wp-content/uploads/nobe_netist_4.jpg" alt="" />
+
+      <Link to="avaleht">
+        <img className="pilt" src="https://estonia.ee/wp-content/uploads/nobe_netist_4.jpg" alt="" />
+      </Link>
+
+      <Link to="ostukorv">
+        <button className="nupp">Ostukorv</button>
+      </Link>
+
+      <Link to="lisa-toode">
+        <button className="nupp">Lisa toode</button>
+      </Link>
+
+      <Routes>
+        <Route path='avaleht' element={ <div>Olen avalehel, mis on nähtav BASE_URL + /avaleht abil</div> } />
+        <Route path='ostukorv' element={ <div>Olen ostukorvis, mis on nähtav localhost:3000/ostukorv</div> } />
+        <Route path='lisa-toode' element={ <div>Olen toote lisamise lehel</div> } />
+        <Route path='*' element={ <div>Not Found leht</div> } />
+      </Routes>
+
     </div>
   );
 }
--- veebipood/src/index.js
+++ veebipood/src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+
+// Navigeerimiseks (Route-miseks)
+// 1. npm install react-router-dom
+// 2. import BrowserRouter
+// 3. BrowserRouter tag panna <App/> ümber
+// 4. App.js failis tekitada URL ja HTML seosed
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
