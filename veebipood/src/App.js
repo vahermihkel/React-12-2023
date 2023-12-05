53b296e2bd351dcdbd01bb5c13df5186412b0bb2
Mihkel Vaher <mihkel.vaher@industry62.com>
2023-12-05 12:21:34 +0200

    html, css

---
+++ veebipood/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="nupp">Nupp</button>
+      <img className="pilt" src="https://estonia.ee/wp-content/uploads/nobe_netist_4.jpg" alt="" />
+    </div>
+  );
+}
+
+export default App;
